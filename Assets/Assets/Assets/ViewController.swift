3d5ed372f0e1780182d790f4ab33f2175fa87969
alexiaruizq <53828050+alexiaruizq@users.noreply.github.com>
2021-09-01 17:22:28 -0700

    Add files via upload

---
+++ Assets/Assets/Assets/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Assets
+//
+//  Created by Alexia Ruiz Quiroz on 01/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var imgSegundaImagen: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+   
+        imgSegundaImagen.image = UIImage(named: "")
+        
+    }
+    
+    
+
+
+}
+
